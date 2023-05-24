2da49d7bdd8f8a227ae2a875d6262ba34105c252
ivo922 <ivo922@email.com>
2023-05-24 12:06:47 +0300

    Add calculator form

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,6 +1,15 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import Heading from '@/components/heading/heading';
+import ProteinCalculator from '@/components/protein-calculator/protein-calculator';
 
 export default function Page() {
-  return <main></main>;
+  return (
+    <>
+      <Heading>
+        <h1>Protein<br />Calculator</h1>
+      </Heading>
+
+      <ProteinCalculator />
+    </>
+  );
 }
---
+++ src/components/protein-calculator/protein-calculator.tsx
@@ -0,0 +1,93 @@
+'use client';
+import React, { useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
+import styles from './protein-calculator.module.css';
+
+const ProteinCalculator = () => {
+  const [result, setResult] = useState(0);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: FieldValues) => {
+    const { price, per100, weight } = data;
+    const result = Number(price) / ((Number(weight) / 100) * Number(per100));
+    setResult(parseFloat(result.toFixed(2)));
+  };
+
+  return (
+    <div className={styles['protein-calculator']}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-body">
+          <div className="form-controls">
+            <label className="form-label" htmlFor="price">
+              Price of the product:
+            </label>
+
+            <input
+              className="form-field"
+              id="price"
+              type="number"
+              step=".01"
+              {...register('price', { required: true })}
+            />
+
+            <div className="form-error">
+              {errors.price && <span>This field is required</span>}
+            </div>
+          </div>
+
+          <div className="form-controls">
+            <label className="form-label" htmlFor="per100">
+              Protein per 100g:
+            </label>
+
+            <input
+              className="form-field"
+              id="per100"
+              type="number"
+              {...register('per100', { required: true })}
+            />
+
+            <div className="form-error">
+              {errors.per100 && <span>This field is required</span>}
+            </div>
+          </div>
+
+          <div className="form-controls">
+            <label className="form-label" htmlFor="weight">
+              Weight of the product in grams:
+            </label>
+
+            <input
+              className="form-field"
+              id="weight"
+              type="number"
+              {...register('weight', { required: true })}
+            />
+
+            <div className="form-error">
+              {errors.weight && <span>This field is required</span>}
+            </div>
+          </div>
+
+          <div className={`${styles['form-actions']} form-actions`}>
+            <button className="button" type="submit">
+              Calculate
+            </button>
+          </div>
+        </div>
+      </form>
+
+      {!!result && (
+        <div className={`${styles.result} form-field`}>
+          <span className="accent">1g of protein = &nbsp;</span> {result} BGN
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProteinCalculator;
